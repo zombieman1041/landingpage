e3e5e646f10b1e1925b91ab010fa9266c4dd8415
Blake Dayman <daymanblake@gmail.com>
2015-05-28 11:56:21 -0700

    finished project page bug where modal duplicates info to other modals

--- js/main.js
+++ js/main.js
@@ -9,3 +9,6 @@ $(".close-panel, #content").click(function(){
 $("html").removeClass("openNav");
   
 });
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
